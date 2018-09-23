f1dbfc3fa57bfdb78ebc3af1c8f870a189adf8cd
DESKTOP-8PGU74B\Andrievs <andrievs@massportal.com>
2018-09-23 17:57:27 +0300

    Proper resistance divider

--- src/ArduinoDUE/Repetier/Extruder.cpp
+++ src/ArduinoDUE/Repetier/Extruder.cpp
@@ -1678,7 +1678,7 @@ void TemperatureController::updateCurrentTemperature()
         currentTemperatureC = ((float)currentTemperature * 500.0f / (1024 << (2 - ANALOG_REDUCE_BITS)));
 #else
 #if FEATURE_CONTROLLER == CONTROLLER_LCD_MP_PHARAOH_DUE
-        currentTemperatureC = ((float)currentTemperature * 500.0f / (1024 << (2 - ANALOG_REDUCE_BITS)));
+        currentTemperatureC = ((float)currentTemperature * 660.0f / (1024 << (2 - ANALOG_REDUCE_BITS)));
 #else
         currentTemperatureC = ((float)currentTemperature * 330.0f / (1024 << (2 - ANALOG_REDUCE_BITS)));
 #endif
